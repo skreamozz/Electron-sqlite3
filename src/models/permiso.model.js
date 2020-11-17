c9cda17bf313aa93622b206132225b525ee6ed4e
skreamozz <skreamozz@gmail.com>
2020-11-16 23:11:04 -0300

    .

--- src/models/permiso.model.js
+++ src/models/permiso.model.js
@@ -3,6 +3,16 @@ const sequelize = require("../database");
 
 class Permisos extends Model {}
 
-Permisos.init();
+Permisos.init(
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    nombre: DataTypes.TEXT,
+    level: DataTypes.INTEGER,
+  },
+  {
+    sequelize,
+    modelName: "permisos",
+  }
+);
 
 module.exports = Permisos;
--- src/models/usuario.model.js
+++ src/models/usuario.model.js
@@ -1,7 +1,28 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../database");
-
+const Permisos = require("./permiso.model");
+const bcrypt = require("bcrypt");
 class Usuarios extends Model {}
-Usuarios.init();
 
+Usuarios.init(
+  {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    user: { type: DataTypes.TEXT, allowNull: false },
+    pass: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      set(value) {
+        const hash = bcrypt.hashSync(value, 10);
+        this.setDataValue("pass", hash);
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: "usuarios",
+  }
+);
+
+Usuarios.belongsTo(Permisos);
+Permisos.hasOne(Usuarios);
 module.exports = Usuarios;
